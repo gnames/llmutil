357f01f149877d7db14ae33a94a84b88ef2eaf5a
Dmitry Mozzherin <dmozzherin@gmail.com>
2023-11-29 20:33:54 +0000

    use slower but better model for embedding

--- llm/embed.py
+++ llm/embed.py
@@ -1,9 +1,9 @@
 from sentence_transformers import SentenceTransformer, util
 
 # vectors 768
-# MODEL = 'multi-qa-mpnet-base-cos-v1'
+MODEL = 'multi-qa-mpnet-base-cos-v1'
 # vectors 384
-MODEL = 'multi-qa-MiniLM-L6-cos-v1'
+# MODEL = 'multi-qa-MiniLM-L6-cos-v1'
 client = SentenceTransformer(MODEL)
 
 
